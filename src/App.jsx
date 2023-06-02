4ebf322baa653af3c66b821021646af85764cedb
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-06-02 12:57:14 +0530

    icons

--- src/App.jsx
+++ src/App.jsx
@@ -122,6 +122,27 @@ function App() {
         </button>
       </div>
       {/* end slider */}
+      {/* icons */}
+      <div className="flex items-center my-5 md:space-x-20 flex-col justify-center md:flex-row">
+        <div className="flex items-center flex-col space-y-3">
+          <img className="w-10" src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/icon-LL-Windows-Logo-80x80?wid=40&hei=40" alt="image" />
+          <a href="#" className="cursor-pointer text-blue-700 underline">Choose your Microsoft 365</a>
+        </div>
+        <div className="flex items-center flex-col space-y-3">
+          <img className="w-10" src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/icon-LL-Tablet-Kickstand-Landscape-Mode-Fluent-80x80?wid=40&hei=40" alt="image" />
+          <a href="#" className="cursor-pointer text-blue-700 underline">Explore Surface devices</a>
+        </div>
+        <div className="flex items-center flex-col space-y-3">
+          <img className="w-10" src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/icon-LL-Xbox-Logo-80x80?wid=40&hei=40" alt="image" />
+          <a href="#" className="cursor-pointer text-blue-700 underline">Buy Xbox games</a>
+        </div>
+        <div className="flex items-center flex-col space-y-3">
+          <img className="w-10" src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/icon-LL-Windows-Logo-80x80?wid=40&hei=40" alt="image" />
+          <a href="#" className="cursor-pointer text-blue-700 underline">Get Windows 11</a>
+        </div>
+      </div>
+      {/* end icons */}
+      
     </>
   );
 }
